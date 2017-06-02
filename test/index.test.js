182fd3fe315dc27febd18522dddc90731a966231
Vlad Holubiev <golubev.vld@gmail.com>
2017-06-02 22:07:41 +0300

    Migrate last tests to jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,3 @@
-const expect = require('chai').expect;
 const fs = require('fs');
 const path = require('path');
 
@@ -6,31 +5,29 @@ const index = require('../index');
 const testHtml = fs.readFileSync(path.resolve(__dirname, 'index.test.html'), 'utf8');
 const testArticleHtml = fs.readFileSync(path.resolve(__dirname, 'lib/article.test.html'), 'utf8');
 
-describe('News Parser', function() {
-  it('should define parseArticle function', function() {
-    expect(index.parseArticle).to.be.a('function');
-  });
+it('should define parseArticle function', () => {
+  expect(index.parseArticle).toBeInstanceOf(Function);
+});
 
-  it('should throw an error if passed empty html', function() {
-    expect(index.parseArticle.bind(this, '')).to.throw(/Empty/);
-  });
+it('should throw an error if passed empty html', () => {
+  expect(index.parseArticle.bind(this, '')).toThrow(/Empty/);
+});
 
-  it('should throw an error if passed html without proper content', function() {
-    expect(index.parseArticle.bind(this, '<html></html>')).to.throw(/Invalid/);
-  });
+it('should throw an error if passed html without proper content', () => {
+  expect(index.parseArticle.bind(this, '<html></html>')).toThrow(/Invalid/);
+});
 
-  it('should return article object', function() {
-    const article = index.parseArticle(testArticleHtml);
-    expect(article).to.be.an('object');
-    expect(article.title).to.have.length.above(10);
-  });
+it('should return article object', () => {
+  const article = index.parseArticle(testArticleHtml);
+  expect(article).toBeInstanceOf(Object);
+  expect(article.title).toMatch(/.{10,}/);
+});
 
-  it('should define parseArticleSnippets function', function() {
-    expect(index.parseArticleSnippets).to.be.a('function');
-  });
+it('should define parseArticleSnippets function', () => {
+  expect(index.parseArticleSnippets).toBeInstanceOf(Function);
+});
 
-  it('should return 15 article snippets', function() {
-    const results = index.parseArticleSnippets(testHtml);
-    expect(results.length).to.equal(15);
-  });
+it('should return 15 article snippets', () => {
+  const results = index.parseArticleSnippets(testHtml);
+  expect(results.length).toBe(15);
 });
