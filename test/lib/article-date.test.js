45c472a72b08bdbdf3db6c4bac3096b9bc428c85
Vlad Holubiev <golubev.vld@gmail.com>
2017-06-02 22:04:44 +0300

    Migrate article date tests to jest

--- test/lib/article-date.test.js
+++ test/lib/article-date.test.js
@@ -1,40 +1,35 @@
-const expect = require('chai').expect;
 const moment = require('moment');
 
 const ArticleDate = require('../../lib/article-date');
 
-describe('News Parser', function() {
-  describe('Article Date Parser', function() {
-    it('should return today`s date if failed to parse', function() {
-      const input = 'whatever';
-      const date = new ArticleDate(input).getDate();
-      const isTodaysDate = moment().diff(date, 'days') === 0;
+it('should return today`s date if failed to parse', () => {
+  const input = 'whatever';
+  const date = new ArticleDate(input).getDate();
+  const isTodaysDate = moment().diff(date, 'days') === 0;
 
-      expect(isTodaysDate).to.be.true;
-    });
+  expect(isTodaysDate).toBeTruthy();
+});
 
-    it('should return date object for date string with Сьогодні', function() {
-      const input = 'Дата: Сьогодні, 09:26 Новини';
-      const date = new ArticleDate(input).getDate();
-      const isTodaysDate = moment().diff(date, 'days') === 0;
+it('should return date object for date string with Сьогодні', () => {
+  const input = 'Дата: Сьогодні, 09:26 Новини';
+  const date = new ArticleDate(input).getDate();
+  const isTodaysDate = moment().diff(date, 'days') === 0;
 
-      expect(isTodaysDate).to.be.true;
-      expect(moment(date).hours()).to.equal(9);
-    });
+  expect(isTodaysDate).toBeTruthy();
+  expect(moment(date).hours()).toBe(9);
+});
 
-    it('should return date object for date string with Вчора', function() {
-      const input = 'Дата: Вчора, 09:26 Новини';
-      const date = new ArticleDate(input).getDate();
-      const isYesterdaysDate = moment().diff(date, 'days') === 1;
+it('should return date object for date string with Вчора', () => {
+  const input = 'Дата: Вчора, 09:26 Новини';
+  const date = new ArticleDate(input).getDate();
+  const isYesterdaysDate = moment().diff(date, 'days') === 1;
 
-      expect(isYesterdaysDate).to.be.true;
-    });
+  expect(isYesterdaysDate).toBeTruthy();
+});
 
-    it('should return date object for regular date string', function() {
-      const input = 'Дата: 15-09-2016, 16:05 Новини';
-      const date = new ArticleDate(input).getDate();
+it('should return date object for regular date string', () => {
+  const input = 'Дата: 15-09-2016, 16:05 Новини';
+  const date = new ArticleDate(input).getDate();
 
-      expect(moment(date).toDate()).to.deep.equal(new Date(2016, 8, 15, 16, 5));
-    });
-  });
+  expect(moment(date).toDate()).toEqual(new Date(2016, 8, 15, 16, 5));
 });
