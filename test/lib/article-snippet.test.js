cf0e1f557138da8adafc4f501e2a75dcae4b7dd7
Vlad Holubiev <golubev.vld@gmail.com>
2017-06-02 22:05:59 +0300

    Migrate article snippet tests to jest

--- test/lib/article-snippet.test.js
+++ test/lib/article-snippet.test.js
@@ -1,36 +1,31 @@
-const expect = require('chai').expect;
 const fs = require('fs');
 const path = require('path');
 
 const testHtml = fs.readFileSync(path.resolve(__dirname, 'article-snippet.test.html'), 'utf8');
 const ArticleSnippet = require('../../lib/article-snippet');
 
-describe('News Parser', function() {
-  describe('Article Snippet Parser', function() {
-    const articleSnippet = new ArticleSnippet(testHtml);
+const articleSnippet = new ArticleSnippet(testHtml);
 
-    it('should return title', function() {
-      expect(articleSnippet.getTitle()).to.contain('Увага! Кредитна');
-    });
+it('should return title', () => {
+  expect(articleSnippet.getTitle()).toMatch('Увага! Кредитна');
+});
 
-    it('should return date', function() {
-      expect(articleSnippet.getDate()).to.be.a('Date');
-    });
+it('should return date', () => {
+  expect(articleSnippet.getDate()).toBeInstanceOf(Date);
+});
 
-    it('should return topic', function() {
-      expect(articleSnippet.getTopic()).to.equal('Міжнародні програми');
-    });
+it('should return topic', () => {
+  expect(articleSnippet.getTopic()).toBe('Міжнародні програми');
+});
 
-    it('should return description', function() {
-      expect(articleSnippet.getDescription()).to.contain('Шановні працівники');
-    });
+it('should return description', () => {
+  expect(articleSnippet.getDescription()).toMatch('Шановні працівники');
+});
 
-    it('should return image link', function() {
-      expect(articleSnippet.getImageLink()).to.contain('51428517078.jpg');
-    });
+it('should return image link', () => {
+  expect(articleSnippet.getImageLink()).toMatch('51428517078.jpg');
+});
 
-    it('should return read more link', function() {
-      expect(articleSnippet.getReadMoreLink()).to.contain('-respublika-polscha.html');
-    });
-  });
+it('should return read more link', () => {
+  expect(articleSnippet.getReadMoreLink()).toMatch('-respublika-polscha.html');
 });
