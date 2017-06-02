36236ad34b33ad77cc005b646952d1a7ca8fc084
Vlad Holubiev <golubev.vld@gmail.com>
2017-06-02 22:03:35 +0300

    Migrate article tests to jest

--- test/lib/article.test.js
+++ test/lib/article.test.js
@@ -1,45 +1,37 @@
-const expect = require('chai').expect;
 const fs = require('fs');
 const path = require('path');
 
 const Article = require('../../lib/article');
 const testHtml = fs.readFileSync(path.resolve(__dirname, 'article.test.html'), 'utf8');
+const article = new Article(testHtml);
 
-describe('News Parser', function() {
-  describe('Article Parser', function() {
-    const article = new Article(testHtml);
+it('should return article title', () => {
+  expect(article.getTitle()).toEqual('Арт-зустріч «НЕформат»');
+});
 
-    it('should return article title', function() {
-      expect(article.getTitle()).to.equal('Арт-зустріч «НЕформат»');
-    });
+it('should return date', () => {
+  expect(article.getDate()).toBeInstanceOf(Date);
+});
 
-    it('should return date', function() {
-      expect(article.getDate()).to.be.a('Date');
-    });
+it('should return author', () => {
+  expect(article.getAuthor()).toMatch('Відділ інформації');
+});
 
-    it('should return author', function() {
-      expect(article.getAuthor()).to.contain('Відділ інформації');
-    });
+it('should return image link', () => {
+  expect(article.getImageLink()).toMatch('1474007227_9wyl4ddx09w.jpg');
+});
 
-    it('should return image link', function() {
-      expect(article.getImageLink()).to.contain('1474007227_9wyl4ddx09w.jpg');
-    });
+it('should return article text', () => {
+  expect(article.getText()).toMatch('молодь');
+});
 
-    it('should return article text', function() {
-      expect(article.getText()).to.contain('молодь');
-    });
+it('should return list of photos', () => {
+  const photos = article.getPhotos();
+  expect(photos).toHaveLength(10);
+});
 
-    it('should return list of photos', function() {
-      const photos = article.getPhotos();
-      expect(photos).to.be.an('array');
-      expect(photos).to.have.length.above(5);
-    });
+it('should return list of attachments', () => {
+  const attachments = article.getAttachments();
 
-    it('should return list of attachments', function() {
-      const attachments = article.getAttachments();
-
-      expect(attachments).to.be.an('array');
-      expect(attachments[0].name).to.contain('лист');
-    });
-  });
+  expect(attachments[0].name).toMatch('лист');
 });
--- wallaby.config.js
+++ wallaby.config.js
@@ -10,7 +10,7 @@ module.exports = function() {
       'test/**/*.test.js'
     ],
 
-    testFramework: 'mocha',
+    testFramework: 'jest',
 
     env: {
       type: 'node'
